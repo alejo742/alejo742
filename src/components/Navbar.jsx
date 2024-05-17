a83fbf6f60173cb7ad094268f38c2e81df78a4ec
alejo742 <a.manrique742@gmail.com>
2024-05-17 10:37:52 -0500

    create: navbar setup

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import './Navbar.css';
+import { useEffect } from 'react';
+import React from 'react';
+
+function Navbar() {
+  return (
+    <div className="main-navbar">
+        
+    </div>
+  );
+}
+
+export default Navbar;
