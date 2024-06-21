b84c393463d18adf3ee089103e75c9894380c0e1
alejo742 <a.manrique742@gmail.com>
2024-06-21 13:16:06 -0500

    add: basic preloader

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import './App.css';
-import React from 'react';
+import {React, useEffect} from 'react';
 import Landing from './pages/Landing.jsx';
 
 function App() {
+  useEffect(() => {
+    window.addEventListener('load', () => {
+      const preloader = document.querySelector('.preloader');
+      const landing = document.querySelector('.landing-wrapper');
+      preloader.classList.add('loaded');
+      landing.classList.add('loaded');
+      setTimeout(() => {
+        preloader.style.display = "none";
+      }, 500);
+    })
+  }, []);
+
   return (
     <div className="App">
+      <div className="preloader">
+        <div className="loader"></div>
+      </div>
       <Landing/>
     </div>
   );
